b3a53ec2f608b8b22c35ec3822b031ef812815ad
WIN-3C2TM68FOU8\Никита <nikitalipatov.25@gmail.com>
2021-09-20 10:26:42 +0300

    Initial commit

---
+++ src/main/java/com/nikitalipatov/trender/TrenderApplication.java
@@ -0,0 +1,13 @@
+package com.nikitalipatov.trender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikitalipatov/trender/TrenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikitalipatov.trender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
